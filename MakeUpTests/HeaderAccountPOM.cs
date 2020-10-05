62b384557cc81536b96cb8f9186ddf1d42249f88
Anna Demchuk <anidemchuk@gmail.com>
2020-10-05 22:48:25 +0300

    Add headerAccountPOM

---
+++ MakeUpTests/HeaderAccountPOM.cs
@@ -0,0 +1,68 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeUpTests
+{
+  public  class HeaderAccountPOM
+    {
+        IWebDriver driver;
+
+        By _cabinet = By.XPath("/html/body/div[1]/div[1]/header/div[1]/div/div[3]/div");
+        By _loginField = By.Name("user_login");
+        By _passwordField = By.Name("user_pw");
+        By _logInButton = By.CssSelector("#form-auth > div > div.form-inner-wrap > div:nth-child(4) > button");
+        By _enter = By.CssSelector("body > div.site-wrap > div.main-wrap > header > div.header-top > div > div:nth-child(3) > a");
+
+        By _registration = By.ClassName("auth-link");
+        public HeaderAccountPOM(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+
+        public HeaderAccountPOM ClickCabinet()
+        {
+            driver.FindElement(_cabinet).Click();
+            return this;
+        }
+
+
+        public HeaderAccountPOM TypeUserLogin(string login)
+        {
+            driver.FindElement(_loginField).SendKeys(login);
+            return this;
+        }
+
+        public HeaderAccountPOM TypeUserPassword(string password)
+        {
+            driver.FindElement(_passwordField).SendKeys(password);
+            return this;
+        }
+
+        
+        public HeaderAccountPOM ClickLogInButton()
+        {
+            driver.FindElement(_logInButton).Click();
+            return this;
+        }
+        
+
+        public string NameAccount()
+        {
+            string nameAccount = driver.FindElement(_enter).Text;
+            return nameAccount;
+        }
+
+        
+        public HeaderAccountPOM ClickRegistration()
+        {
+            driver.FindElement(_registration).Click();
+            return this;
+        }
+
+    }
+}
