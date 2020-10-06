03d3f413d8674e3d6719884c917b79635d94ff27
Олександр Owl <aischenko99@gmail.com>
2020-10-06 10:28:44 +0300

    resolve isssue

--- MakeUpTests/POMs/CatalogPOM.cs
+++ MakeUpTests/POMs/CatalogPOM.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
 
-namespace MakeUpTests.POMs
+namespace MakeUpTests
 {
-    class CatalogPOM
+    public class CatalogPOM
     {
+        IWebDriver driver;
+
+        public CatalogPOM(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        By _pageheader = By.CssSelector("body > div.site-wrap > div.main-wrap > div > div > div:nth-child(2) > h1");
+        By _breadcrumbs = By.CssSelector("body > div.site-wrap > div.main-wrap > div > div > div:nth-child(2) > h1");
+        By _productItemDescription = By.CssSelector("body > div.site-wrap > div.main-wrap > div > div > div:nth-child(2) > div.catalog > div.catalog-content > div > div.catalog-products > ul > li:nth-child(1) > div.simple-slider-list__link > div.info-product-wrapper > div.simple-slider-list__description");
+
+        public bool HasTextPageHeader(string nameProduct)
+        {
+            string text = driver.FindElement(_pageheader).Text;
+            return text.Contains(nameProduct);
+        }
+
+        public bool HasProduct(string nameProduct)
+        {
+            string text = driver.FindElement(_productItemDescription).Text;
+            return text.Contains(nameProduct);
+        }
+
     }
 }
